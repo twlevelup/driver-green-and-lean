cddcc8a7ce6fd6f5733fcd54416cd77d4da47681
Charles Korn <charles@2mar.com>
2014-08-12 20:50:24 +1000

    Implemented first story (moving forward). Enabled colourful test output from Rake.

---
+++ lib/car.rb
@@ -0,0 +1,30 @@
+class Car
+	attr_reader :x, :y, :orientation
+
+  def initialize(x, y, orientation)
+  	if ![:north, :east, :south, :west].include?(orientation)
+  		raise Exception('Invalid orientation.')
+  	end
+
+  	@x = x
+  	@y = y
+  	@orientation = orientation
+  end
+    
+  def move_forward
+		case @orientation
+	  	when :north
+				@y = @y + 1
+	  	when :south
+				@y = @y - 1
+	  	when :east
+	  		@x = @x + 1
+	  	when :west
+	  		@x = @x - 1
+  	end
+  end
+
+  def position
+  	[@x, @y, @orientation]
+  end
+end
---
+++ spec/car_spec.rb
@@ -0,0 +1,90 @@
+require 'car'
+
+RSpec.describe Car do
+
+  context 'when a car starts at (1, 1, N)' do
+  	before(:each) do
+	  	@d = Car.new(1, 1, :north)
+	  end
+
+	  it 'should report its position as (1, 1) and be pointing north' do 
+  		expect(@d.position).to eq([1, 1, :north])
+  	end
+
+	  context 'and it moves forward' do
+	  	before(:each) do
+		  	@d.move_forward
+		  end
+
+		  it 'should move the car to (1, 2) and still be pointing north' do 
+	  		expect(@d.position).to eq([1, 2, :north])
+	  	end
+	  end
+  end
+
+  context 'when a car starts at (1, 1), is pointing south and moves forward' do
+  	before(:each) do
+	  	@d = Car.new(1, 1, :south)
+	  	@d.move_forward
+	  end
+
+  	it 'should move the car to (1, 0) and still be pointing south' do 
+  		expect(@d.position).to eq([1, 0, :south])
+  	end
+  end
+
+  context 'when a car starts at (1, 2), is pointing north and moves forward' do 
+  	before(:each) do
+  		@d = Car.new(1, 2, :north)
+  		@d.move_forward
+  	end
+
+  	it 'should move the car to (1, 3) and still be pointing north' do
+  		expect(@d.position).to eq([1, 3, :north])
+  	end
+  end
+
+  context 'when a car starts at (1, 2), is pointing south and moves forward' do 
+  	before(:each) do
+  		@d = Car.new(1, 2, :south)
+  		@d.move_forward
+  	end
+
+  	it 'should move the car to (1, 1) and still be pointing south' do
+  		expect(@d.position).to eq([1, 1, :south])
+  	end
+  end
+
+  context 'when a car starts at (1, 1), is pointing east and moves forward' do 
+  	before(:each) do
+  		@d = Car.new(1, 1, :east)
+  		@d.move_forward
+  	end
+
+  	it 'should move the car to (2, 1) and still be pointing east' do
+  		expect(@d.position).to eq([2, 1, :east])
+  	end
+  end
+
+  context 'when a car starts at (1, 1), is pointing west and moves forward' do 
+  	before(:each) do
+  		@d = Car.new(1, 1, :west)
+  		@d.move_forward
+  	end
+
+  	it 'should move the car to (0, 1) and still be pointing west' do
+  		expect(@d.position).to eq([0, 1, :west])
+  	end
+  end
+
+  it 'should not allow invalid starting orientations' do
+  	expect { Car.new(1, 1, :rubbish) }.to raise_error 
+  end
+end
+
+
+
+
+
+
+
