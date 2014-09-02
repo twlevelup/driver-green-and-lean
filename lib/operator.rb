10c4e1dd2b2ab5aa30b1c8e3f421773a4dc3831d
Jackson Chan <jackosmacko@gmail.com>
2014-09-02 19:33:34 +1000

    Console functions for Operator and test

--- lib/operator.rb
+++ lib/operator.rb
@@ -1,14 +1,76 @@
+require "car"
+
 class Operator 
 
-	def initialize()
-		set_location
-	end
+attr_reader :x, :y, :orientation
+		
+		def initialize
 
-	def set_location
-		puts "Please enter a car location and orientation"
-		STDOUT.flush
-		location = gets.chomp
-		puts "Car location is at " + location
-	end
+			@x
+			@y
+			@orientation
+		end
+
+		def set_location 
+			puts "Please enter a car location and orientation"
+			STDOUT.flush
+
+			location = STDIN.gets.chomp
+
+			points = location.split(',')
+
+			@x = points[0]
+			@y = points[1]
+			@orientation = points[2]
+
+			@x = @x.to_i
+			@y = @y.to_i
+
+			@orientation = @orientation.intern
+
+			self.create_car(@x, @y, @orientation)
+
+			[@x, @y, @orientation]
+
+			end
+	 
+
+		def create_car(x, y, orientation)
+
+			@car = Car.new(x, y, orientation)
+
+			puts "New car created : #{@car.position} " 
+
+		end
+
+		def instruction_stack(car)
+
+			puts "Please enter instructions for car:"
+
+			instructions = STDIN.gets.chomp
+
+			moves = instructions.split("")
+
+			moves.each { |x| self.move_car?(x) }
+
+			@car.position
+
+		end
+
+		def move_car?(instruction)
+
+			@instruction = instruction.intern
+
+			case @instruction
+				when :F
+					@car.move_forward
+				when :B
+					@car.move_backward
+				when :L
+					@car.turn_left
+				when :R
+					@car.turn_right
+				end
+		end
 
 end
--- spec/operator_spec.rb
+++ spec/operator_spec.rb
@@ -5,7 +5,17 @@ describe Operator do
 		it 'should accept the input from operator and display the inputs' do
 			@operator = Operator.new
 
-			#expect(@operator.set_location).to eq([00E])
+			expect(@operator.set_location).to eq([0, 0, :north])
 		end
+
+		it 'should accept list of instructions and move car to final destination case 1' do
+
+			@operator = Operator.new
+
+			@operator.set_location
+
+			expect(@operator.instruction_stack(@car)).to eq([3, 2, :north])
+		end
+
 	end
 end
